de23ec7837bcca0783cc5b76cac32eb84e9d40c8
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-11-11 14:38:03 -0300

    :lipstick: feat: Inicia desenvolvimento da sidebar

--- src/app/index/barra-lateral/barra-lateral.component.ts
+++ src/app/index/barra-lateral/barra-lateral.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import {
+  faHouse,
+  faPlay,
+  faDisplay,
+  faClockRotateLeft,
+  faBarsStaggered,
+  faVideo,
+  faClock,
+  faThumbsUp,
+  faFire,
+  faShoppingBag,
+  faMusic,
+  faClapperboard,
+  faTowerBroadcast,
+  faGamepad,
+  faNewspaper,
+  faTrophy,
+  faLightbulb,
+  faPodcast,
+  faGear,
+  faFlag,
+  faCircleQuestion,
+  faExclamation,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-barra-lateral',
   templateUrl: './barra-lateral.component.html',
-  styleUrl: './barra-lateral.component.css'
+  styleUrl: './barra-lateral.component.css',
 })
 export class BarraLateralComponent {
-
+  house = faHouse;
+  play = faPlay;
+  display = faDisplay;
+  clockRotateLeft = faClockRotateLeft;
+  barsStaggered = faBarsStaggered;
+  video = faVideo;
+  clock = faClock;
+  thumbsUp = faThumbsUp;
+  fire = faFire;
+  shoppingBag = faShoppingBag;
+  music = faMusic;
+  clapperboard = faClapperboard;
+  towerBroadcast = faTowerBroadcast;
+  gamepad = faGamepad;
+  newspaper = faNewspaper;
+  trophy = faTrophy;
+  lightbulb = faLightbulb;
+  podcast = faPodcast;
+  gear = faGear;
+  flag = faFlag;
+  circleQuestion = faCircleQuestion;
+  exclamation = faExclamation;
 }
