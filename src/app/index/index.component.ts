55268b9f68366d2fd907ac5f4a33acffb5aa3858
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-11-12 09:36:54 -0300

    :wrench: feat: Ajusta exibição do layout

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -8,4 +8,28 @@ import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 })
 export class IndexComponent {
   options = faEllipsisVertical;
+
+  navBarTop = [
+    'Rock',
+    'Pop',
+    'Jazz',
+    'Sci-Fi',
+    'Action',
+    'Thriller',
+    'Horror',
+    'RPG',
+    'Adventure',
+    'Indie Games',
+    'Classical Music',
+    'Fiction Movies',
+    'Strategy',
+    'Soundtracks',
+    'Reggae',
+    'Documentaries',
+    'Puzzle Games',
+    'K-Pop',
+    'Superheroes',
+  ];
+
+  videos = [1, 2, 3, 4, 5, 6];
 }
