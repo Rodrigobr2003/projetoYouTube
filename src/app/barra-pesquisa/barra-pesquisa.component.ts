5c8879286f840ee03da05b49a07e3e190f3208fe
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-11-12 16:22:41 -0300

    :lipstick: feat: Prepara estrutura searchbar

--- src/app/barra-pesquisa/barra-pesquisa.component.ts
+++ src/app/barra-pesquisa/barra-pesquisa.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import {
+  faBars,
+  faBell,
+  faCamera,
+  faMagnifyingGlass,
+  faMicrophone,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-barra-pesquisa',
   templateUrl: './barra-pesquisa.component.html',
-  styleUrl: './barra-pesquisa.component.css'
+  styleUrl: './barra-pesquisa.component.css',
 })
 export class BarraPesquisaComponent {
-
+  bars = faBars;
+  microphone = faMicrophone;
+  camera = faCamera;
+  bell = faBell;
+  search = faMagnifyingGlass;
 }
