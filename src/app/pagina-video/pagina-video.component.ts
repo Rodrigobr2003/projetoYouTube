dc13ad91fecbfd6c0c87eaf02b67d90315d64420
Rodrigobr2003 <ronaldobr2003@gmail.com>
2024-11-14 13:37:09 -0300

    :sparkles: feat: Adiciona conteudo página vídeo

--- src/app/pagina-video/pagina-video.component.ts
+++ src/app/pagina-video/pagina-video.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import {
+  faAngleDown,
+  faBars,
+  faEllipsis,
+  faEllipsisVertical,
+  faShare,
+  faThumbsDown,
+  faThumbsUp,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-pagina-video',
   templateUrl: './pagina-video.component.html',
-  styleUrl: './pagina-video.component.css'
+  styleUrl: './pagina-video.component.css',
 })
 export class PaginaVideoComponent {
-
+  thumbsUp = faThumbsUp;
+  thumbsDown = faThumbsDown;
+  share = faShare;
+  option = faEllipsisVertical;
+  bars = faBars;
+  angleDown = faAngleDown;
 }
